ed711d5f017b7262bab99ef7f58956c4b43382de
lai <1164258202@qq.com>
2021-06-15 10:58:42 +0800

    a

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -4,27 +4,37 @@ import 'package:web_demo/pages/about_page.dart';
 import 'package:web_demo/pages/article_detail_page.dart';
 import 'package:web_demo/pages/category_articles_page.dart';
 import 'package:web_demo/pages/home_page.dart';
-import 'package:page_transition/page_transition.dart';
 
 class Router {
   final RoutemasterParser parser = RoutemasterParser();
 
   final RoutemasterDelegate routemaster = RoutemasterDelegate(
+    transitionDelegate: NoAnimationTransitionDelegate(),
     routesBuilder: (context) => RouteMap(
-      routes: {
-        '/': (_) => MaterialPage(child: HomePage()),
-        '/about': (_) => MaterialPage(child: AboutPage()),
-        '/detail/:id': (info) => PageTransition(
-            child: ArticleDetailPage(articleId: info.queryParameters['id']!),
-            type: PageTransitionType.fade) as Page,
-        '/categoryArticles/:id': (info) => MaterialPage(
-              child: CategoryArticlesPage(
-                categoryId: info.pathParameters['id']!,
-                params: info.queryParameters,
+        routes: {
+          '/': (_) => MaterialPage(child: HomePage()),
+          '/about': (_) => MaterialPage(child: AboutPage()),
+          '/detail/:id': (info) => MaterialPage(
+              child: ArticleDetailPage(articleId: info.pathParameters['id']!)),
+          '/categoryArticles/:id': (info) => MaterialPage(
+                child: CategoryArticlesPage(
+                  categoryId: info.pathParameters['id']!,
+                  params: info.queryParameters,
+                ),
+              ),
+        },
+        onUnknownRoute: (path) {
+          return MaterialPage(
+            child: Scaffold(
+              body: Center(
+                child: Text(
+                  "Couldn't find page '$path'",
+                  style: Theme.of(context).textTheme.headline3,
+                ),
               ),
             ),
-      },
-    ),
+          );
+        }),
   );
 
   NavigationResult<T> push<T extends Object?>(
@@ -32,5 +42,39 @@ class Router {
     Map<String, String>? queryParameters,
   }) {
     return routemaster.push<T>(path, queryParameters: queryParameters);
+  }
+}
+
+class NoAnimationTransitionDelegate extends TransitionDelegate<void> {
+  @override
+  Iterable<RouteTransitionRecord> resolve({
+    required List<RouteTransitionRecord> newPageRouteHistory,
+    required Map<RouteTransitionRecord?, RouteTransitionRecord>
+        locationToExitingPageRoute,
+    Map<RouteTransitionRecord?, List<RouteTransitionRecord>>?
+        pageRouteToPagelessRoutes,
+  }) {
+    final results = <RouteTransitionRecord>[];
+
+    for (final pageRoute in newPageRouteHistory) {
+      if (pageRoute.isWaitingForEnteringDecision) {
+        pageRoute.markForAdd();
+      }
+      results.add(pageRoute);
+    }
+
+    for (final exitingPageRoute in locationToExitingPageRoute.values) {
+      if (exitingPageRoute.isWaitingForExitingDecision) {
+        exitingPageRoute.markForRemove();
+        final pagelessRoutes = pageRouteToPagelessRoutes![exitingPageRoute];
+        if (pagelessRoutes != null) {
+          for (final pagelessRoute in pagelessRoutes) {
+            pagelessRoute.markForRemove();
+          }
+        }
+      }
+      results.add(exitingPageRoute);
+    }
+    return results;
   }
 }
